bf734b046f5b8d9dea9ac28dc67b27cc958e6128
MickHardins <mick@MacBook-Pro-di-Mick.local>
2017-01-20 19:48:48 +0100

    a

--- src/com/evilbox/Request/RequestSender.java
+++ src/com/evilbox/Request/RequestSender.java
@@ -28,6 +28,7 @@ public class RequestSender {
     private boolean forceReAnalysis;
     private VirustotalPublicV2 virustotalPublicV2;
     private String zipPassword;
+    private String prova_branching;
 
 
     /**
